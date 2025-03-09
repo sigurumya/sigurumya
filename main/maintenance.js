b1c4ac6ac92bbc8776a081774556d4ee3c0fad16
sigurumya <sigurumya@gmail.com>
2025-03-09 04:09:41 +0000

    色々修正しました

--- main/maintenance.js
+++ main/maintenance.js
@@ -1,15 +1,19 @@
 window.addEventListener("load", () => {
-  fetch("maintenance.json")
-    .then((resp) => resp.json())
-    .then((json) => {
-      const info = document.getElementById("time");
-      info.innerHTML = ""; // 既存の内容をクリア
-      json.forEach((item) => {
-        info.innerHTML += `${item.time}`;
+  const fetchData = () => {
+    fetch("maintenance.json")
+      .then((resp) => resp.json())
+      .then((json) => {
+        const info = document.getElementById("time");
+        info.innerHTML = "";
+        json.forEach((item) => {
+          info.innerHTML += `${item.time}`;
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching maintenance data:", error);
       });
-    })
-    .catch((error) => {
-      console.error("Error fetching maintenance data:", error);
-    });
+  };
+  fetchData();
+  setInterval(fetchData, 30000);
 });
 window.location.assign("https://sigurumya.glitch.me/index.html");
